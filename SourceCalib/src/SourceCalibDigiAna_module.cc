9118fff02a8a812bff9b8f49c36fc55e2f31fc52
sophieMu2e <sophie@fnal.gov>
2024-06-21 10:20:59 -0500

    updated analyzer

--- SourceCalib/src/SourceCalibDigiAna_module.cc
+++ SourceCalib/src/SourceCalibDigiAna_module.cc
@@ -46,6 +46,8 @@ namespace mu2e {
            fhicl::Atom<std::string>                            processorStrategy   { Name("processorStrategy"),   Comment("Digi reco processor name") };
            fhicl::Atom<double>                                 digiSampling        { Name("digiSampling"),        Comment("Calo ADC sampling time (ns)") };
            fhicl::Atom<double>                                 maxChi2Cut          { Name("maxChi2Cut"),          Comment("Chi2 cut for keeping reco digi") };
+           fhicl::Atom<double>                                 ratioCut          { Name("ratioCut"),          Comment("ratio of energy in crystal to others"), 0.8 };
+           fhicl::Atom<double>                                timeCut          { Name("timeCut"),          Comment("time for digis in same event"), 20 };
            fhicl::Atom<int>                                    diagLevel           { Name("diagLevel"),           Comment("Diagnosis level") };
         };
 
@@ -56,6 +58,8 @@ namespace mu2e {
            processorStrategy_ (config().processorStrategy()),
            digiSampling_      (config().digiSampling()),
            maxChi2Cut_        (config().maxChi2Cut()),
+           ratioCut_          (config().ratioCut()),
+           timeCut_          (config().timeCut()),
            diagLevel_         (config().diagLevel())
         {
             std::map<std::string, processorStrategy> spmap;
@@ -93,6 +97,8 @@ namespace mu2e {
         const  std::string                           processorStrategy_;
         double                                       digiSampling_;
         double                                       maxChi2Cut_;
+        double                                       ratioCut_;
+        double                                       timeCut_;
         int                                          diagLevel_;
         std::unique_ptr<CaloWaveformProcessor>       waveformProcessor_;
         TH1F* list_of_hists[ncrystals];
@@ -132,14 +138,20 @@ namespace mu2e {
 
       double totEnergyReco(0);
       std::vector<double> x{},y{};
+      
+      //int idExist = 0;
+      std::vector<int> crystals_in_event;
+      std::vector<double> time;
+      double total_energy_in_crystal[ncrystals];
 
       for (const auto& caloDigi : caloDigis)
       {
           int    SiPMID   = caloDigi.SiPMID();
           double t0       = caloDigi.t0();
+          time.push_back(t0);
           const std::vector<int>& waveform = caloDigi.waveform();
-          size_t index = &caloDigi - &caloDigis.front();
-          art::Ptr<CaloDigi> caloDigiPtr(caloDigisHandle, index);
+          //size_t index = &caloDigi - &caloDigis.front();
+          //art::Ptr<CaloDigi> caloDigiPtr(caloDigisHandle, index);
 
           x.clear();y.clear();
           for (unsigned int i=0;i<waveform.size();++i)
@@ -150,7 +162,7 @@ namespace mu2e {
 
           waveformProcessor_->reset();
           waveformProcessor_->extract(x,y);
-          if (diagLevel_ > 2) std::cout<<"SourceCalibDigiAna found "<<waveformProcessor_->nPeaks()<<" peaks for SiPMID="<<SiPMID<<std::endl;
+
           for (int i=0;i<waveformProcessor_->nPeaks();++i)
           {
               double chi2      = waveformProcessor_->chi2();
@@ -158,8 +170,42 @@ namespace mu2e {
               double eDep      = waveformProcessor_->amplitude(i);
               if (SiPMID%2==0) totEnergyReco += eDep;
               if (chi2/float(ndf) > maxChi2Cut_) continue;
-              list_of_hists[SiPMID/2]->Fill(waveformProcessor_->amplitude(i));
+              
+              if(crystals_in_event.size() !=0 ) {
+                if (Contains(crystals_in_event, SiPMID/2) == 0) {
+                  crystals_in_event.push_back(SiPMID/2);
+                  total_energy_in_crystal[SiPMID/2] = eDep;
+                } else{ total_energy_in_crystal[SiPMID/2]+= eDep; }
+              } else crystals_in_event.push_back(SiPMID/2);
+              //list_of_hists[SiPMID/2]->Fill(waveformProcessor_->amplitude(i));
           }
+      }
+      
+      bool passes_time = true; 
+      bool passes_ratio = true;
+      double difTime = 0;
+      if(time.size() !=0) {
+        sort(time.begin(), time.end());
+        difTime = time.back() - time.front();
+      }
+      for(const auto& id : crystals_in_event){
+        float edepTarget = 0.0;
+        float edepOthers   = 0.0;
+        for(const auto& id2 : crystals_in_event){
+          if(id == id2) edepTarget += total_energy_in_crystal[id];
+          else edepOthers += total_energy_in_crystal[id];
+        }
+        std::cout<<difTime<<std::endl;
+        if(difTime > timeCut_){
+          passes_time = false;
+        }
+
+        if(edepTarget / (edepTarget + edepOthers) < ratioCut_){
+          passes_ratio = false;
+        }
+        if(passes_time and passes_ratio){
+          list_of_hists[id]->Fill(total_energy_in_crystal[id]);
+        }
       }
       if (diagLevel_ > 1) std::cout<<"[SourceCalibDigiAna] Total energy reco "<<totEnergyReco <<std::endl;
   }
