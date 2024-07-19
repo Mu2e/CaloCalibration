c48c6e6fd5c780251220153b504840f71d27fb16
Pierluigi Fedeli <pfedeli@mu2egpvm01.fnal.gov>
2024-07-19 05:42:04 -0500

    added studies on photoelectrons

--- CosmicsCalib/src/CaloCosmicEnergy_module.cc
+++ CosmicsCalib/src/CaloCosmicEnergy_module.cc
@@ -31,6 +31,8 @@
 #include "Offline/RecoDataProducts/inc/CaloCluster.hh"
 #include "Offline/DataProducts/inc/CaloConst.hh"
 
+#include "messagefacility/MessageLogger/MessageLogger.h"
+
 //C++ includes
 #include <string>
 #include <iostream>
@@ -39,6 +41,7 @@
 
 //ROOT includes
 #include "TH1F.h"
+#include "TH2F.h"
 #include "TF1.h"
 #include "TGraph.h"
 #include "TGraphErrors.h"
@@ -63,12 +66,22 @@ namespace mu2e{
     float findpath(float m,float q, float x, float y);
 
   private:
+  
+    art::ServiceHandle<art::TFileService> tfs;
+    art::TFileDirectory tfdir = tfs->mkdir("All_tracks");
+    art::TFileDirectory tfdirv = tfs->mkdir("Vertical_tracks");
+    art::TFileDirectory tfdird = tfs->mkdir("Diagonal_tracks");
+    art::TFileDirectory tfdirfp = tfs->mkdir("All_normalized_tracks");
+    art::TFileDirectory tfdir_res = tfs->mkdir("MPV_distributions");
+    art::TFileDirectory tfdir_cryalr = tfs->mkdir("Asymmetry_lr_Crystals");
+    art::TFileDirectory tfdir_alr = tfs->mkdir("Analysis_asymmetry_lr");
 
     //calo parameters
     static constexpr int nSiPMs = CaloConst::_nSiPMPerCrystal;
     static constexpr int nCrystals = CaloConst::_nCrystalPerDisk;
     static constexpr int nDisks = CaloConst::_nDisk;
     static constexpr int nROchan = nDisks*nCrystals*nSiPMs;
+    static constexpr int Ebin = 11; //number of 5 MeV ranges for the sigma asymmetry variable
     
     //fcl parameters
     int                   _diagLevel;
@@ -82,10 +95,16 @@ namespace mu2e{
     TH1F *hSiPMv[nDisks*nCrystals][nSiPMs];
     TH1F *hSiPMd[nDisks*nCrystals][nSiPMs];
     TH1F *hSiPMfp[nDisks*nCrystals][nSiPMs];
-    TH1F *MPV,*MPVv,*MPVd,*MPVfp;
+   
+    TH1F *MPV,*MPVv,*MPVd,*MPVfp,*LR_all, *LR[Ebin], *ALR[Ebin], *CryALR[nDisks*nCrystals][Ebin], *CryNpe, *CrySigNoise, *CryNpeChi2;
+    TH2F *ALR_all;
+    float Energy_band[Ebin],counter_energy_band[Ebin], Cry_Energy_band[nDisks*nCrystals][Ebin], Cry_counter_energy_band[nDisks*nCrystals][Ebin];
+    TF1* gaussianFit = new TF1("gaussianFit", "gaus");
 
 
     std::ofstream              _outputfile;
+    std::ofstream              _outputfileAsym;
+
 
     //helper variables
     int ncry =0; // need to count how many crystals pass the selections
@@ -117,30 +136,28 @@ namespace mu2e{
 
   void CaloCosmicEnergy::beginJob(){
     if (_diagLevel > 0 ) std::cout<< "CaloCosmicEnergy: Entering beginJob" << std::endl;
-   
-    art::ServiceHandle<art::TFileService> tfs;
-    art::TFileDirectory tfdir = tfs->mkdir("All tracks");
-    art::TFileDirectory tfdirv = tfs->mkdir("Vertical tracks");
-    art::TFileDirectory tfdird = tfs->mkdir("Diagonal tracks");
-    art::TFileDirectory tfdirfp = tfs->mkdir("All normalized tracks");
-    art::TFileDirectory tfdir_res = tfs->mkdir("MPV distributions");
-
+ 
     for(int iCry=0; iCry < nDisks*nCrystals; iCry++){
       for(int iSiPM=0; iSiPM < nSiPMs; iSiPM++){
 	hSiPM[iCry][iSiPM] = tfdir.make<TH1F>(Form("Crystal_%i_SiPM_%i",iCry, iSiPM),Form("Energy deposited in SiPM %i crystal %d",iSiPM, iCry),70, 15., 50.);
 	hSiPMv[iCry][iSiPM] = tfdirv.make<TH1F>(Form("Crystal_%i_SiPM_%i_v",iCry, iSiPM),Form("Energy deposited in SiPM %i crystal %d only vertical tracks",iSiPM, iCry),70, 15., 50.);
 	hSiPMd[iCry][iSiPM] = tfdird.make<TH1F>(Form("Crystal_%i_SiPM_%i_d",iCry, iSiPM),Form("Energy deposited in SiPM %i crystal %d only diagonal tracks",iSiPM, iCry),70, 15., 50.);
-	hSiPMfp[iCry][iSiPM] = tfdirfp.make<TH1F>(Form("Crystal_%i_SiPM_%i_fp",iCry, iSiPM),Form("Energy deposited in SiPM %i crystal %d normalized track lenght",iSiPM, iCry),70, 15., 50.);
+	hSiPMfp[iCry][iSiPM] = tfdirfp.make<TH1F>(Form("Crystal_%i_SiPM_%i_fp",iCry, iSiPM),Form("Energy deposited in SiPM %i crystal %d normalized track length",iSiPM, iCry),70, 15., 50.);
+      }
+      for(int ibin=0; ibin < Ebin; ibin++){
+	CryALR[iCry][ibin] = new TH1F(Form("Asymmetry_Crystal_%i_band_%i", iCry, ibin), Form("Asymmetry of crystal %i in band [%i, %i)",iCry, (ibin+1)*5, (ibin+1)*5+5), 200,-1., 1.);
       }
     }
     MPV = tfdir_res.make<TH1F>("MPV_distr","MPV distribution",400, 10., 30.);
     MPVv = tfdir_res.make<TH1F>("MPV_v_distr","MPV distribution only vertical tracks",400, 10., 30.);
     MPVd = tfdir_res.make<TH1F>("MPV_d_distr","MPV distribution only diagonal tracks",400, 10., 30.);
     MPVfp = tfdir_res.make<TH1F>("MPV_fp_distr","MPV distribution all normalized tracks",400, 10., 30.);
-
-
-    _outputfile.open("calibration_parameters.dat", std::ios::out);
-
+    ALR_all = tfdir_alr.make<TH2F>("ALR","Asymmetry left right all energies",600,0.,600., 100, -1., 1.);
+    LR_all = tfdir_alr.make<TH1F>("ALR_All_Energies", "Asymmetry Left Right all energies", 200, -1, 1.);
+    for(int i =0; i<Ebin; i++){
+      LR[i] = tfdir_alr.make<TH1F>(Form("LR_ene_band_%i",i), Form("Ratio Left Right Energy band [%i - %i) MeV", i*5, (i+1)*5), 100, 0.5, 1.5);
+      ALR[i] = tfdir_alr.make<TH1F>(Form("ALR_ene_band_%i",i), Form("Asymmetry Left-Right Energy band [%i - %i) MeV", i*5, (i+1)*5), 200, -1., 1.);
+    }
   }//end begin job
 
 
@@ -282,16 +299,39 @@ namespace mu2e{
 	    }
 	  }
 
-	  //cut on vertical tracks + good fit chi2
+	  //cut on vertical tracks + good fit chi2 -> all tracks
 	  if(Dx < 35 || chi2norm < CutChi2Norm ){
 	    for(int iCry=0; iCry< ncry; iCry++){
 	      for(int iSiPM =0; iSiPM< nSiPMs; iSiPM++){
 		hSiPM[IDs[iCry]][iSiPM]->Fill(caloClusters[iClu].caloHitsPtrVector()[whichHit[iCry]].get()->recoCaloDigis()[iSiPM].get()->energyDep());
+		
+		//normalized tracks by fidpaths.h
 		hSiPMfp[IDs[iCry]][iSiPM]->Fill(caloClusters[iClu].caloHitsPtrVector()[whichHit[iCry]].get()->recoCaloDigis()[iSiPM].get()->energyDep()*34.3/path[iCry]);
 	      
 	      }
-	    }
+      	    }
 	  }// chi2 cut
+	  
+	  //asymmetry left-right and ratio left/right->  we use only trigger selection 
+	  for(long unsigned int iCry=0 ;iCry <caloClusters[iClu].caloHitsPtrVector().size(); iCry++){
+	    float left_sipm = caloClusters[iClu].caloHitsPtrVector()[iCry].get()->recoCaloDigis()[0].get()->energyDep();
+	    float right_sipm = caloClusters[iClu].caloHitsPtrVector()[iCry].get()->recoCaloDigis()[1].get()->energyDep(); 
+	    float sipm_mean_e = caloClusters[iClu].caloHitsPtrVector()[iCry].get()->energyDep();
+	    int crystal_id = caloClusters[iClu].caloHitsPtrVector()[iCry].get()->crystalID();
+
+	    LR_all->Fill((left_sipm - right_sipm)/ (left_sipm + right_sipm));      
+	    ALR_all->Fill(sipm_mean_e, (left_sipm - right_sipm)/ (left_sipm + right_sipm));
+	    if(sipm_mean_e <= 55.){
+	      int whichband = sipm_mean_e/5;
+	      Energy_band[whichband]+=sipm_mean_e;
+	      counter_energy_band[whichband]++;
+	      LR[whichband]->Fill(left_sipm/right_sipm);
+	      ALR[whichband]->Fill((left_sipm - right_sipm)/ (left_sipm + right_sipm));
+	      CryALR[crystal_id][whichband]->Fill((left_sipm - right_sipm)/ (left_sipm + right_sipm));
+	      Cry_Energy_band[crystal_id][whichband]+=sipm_mean_e;
+	      Cry_counter_energy_band[crystal_id][whichband]++;
+	    }
+	  }
 
 	  delete gfit;
 	  delete gfirst; 
@@ -303,6 +343,21 @@ namespace mu2e{
 
   void CaloCosmicEnergy::endJob(){
     std::cout << "CaloCosmicEnergy: entering endjob" << std::endl;
+    TGraphErrors *NpeLR, *NpeALR, *sigmaELR, *sigmaEALR, *CryNpeALR[nDisks*nCrystals];
+
+
+    //opening file.dat
+
+    std::string outDir = std::getenv("OUTDIR");
+    if(outDir.length()==0){
+      mf::LogError("OUTDIR-NOT-SET")
+	<< "Environmental variable for calib output file not set" << std::endl;
+    }
+    std::string calibfname = outDir + "/calib_parameters.dat";
+    std::string calibfnameAsym = outDir + "/npe_noise.dat";
+
+    _outputfile.open(calibfname , std::ios::out);
+    _outputfileAsym.open(calibfnameAsym , std::ios::out);
 
     //fit params
     int redo[nDisks*nCrystals][nSiPMs];
@@ -393,7 +448,7 @@ namespace mu2e{
     }
 
     _outputfile.close();
-
+    
 
     //fits for vertical diagonal and nromalized tracks
     for(int iCry=0; iCry< nDisks*nCrystals; iCry++){
@@ -427,10 +482,124 @@ namespace mu2e{
 
       }
     }
-    MPV->Fit("gaus","Q","",10,30.);
-    MPVv->Fit("gaus","Q","",10,30.);
-    MPVd->Fit("gaus","Q","",10,30.);
-    MPVfp->Fit("gaus","Q","",10,30.);
+    MPV->Fit("gaus","Q","",10.,30.);
+    MPVv->Fit("gaus","Q","",10.,30.);
+    MPVd->Fit("gaus","Q","",10.,30.);
+    MPVfp->Fit("gaus","Q","",10.,30.);
+
+    //start Asymmetry L-R analysis
+
+    LR_all->Fit("gaus", "Q", "",-1.,1.);
+    
+    //helper variables
+    float sigmaLR[Ebin],sigmaLRerr[Ebin], sigmaALR[Ebin], sigmaALRerr[Ebin], NpeLRvar[Ebin], NpeALRvar[Ebin], eband_mean[Ebin], errEne[Ebin], errLR[Ebin], errALR[Ebin], CrySigmaALR[nDisks*nCrystals][Ebin], CrySigmaALRErr[nDisks*nCrystals][Ebin], Cryeband_mean[nDisks*nCrystals][Ebin], CryEneErr[nDisks*nCrystals][Ebin];
+
+    //function to fit Asymmetry L-R(ALR) and L/R(LR)
+    TF1 *functional_form_ALR = new TF1("functional_form_ALR", "TMath::Sqrt(0.5*(1/([0]*x)+([1]/x)^2))", 0., 100.);
+    TF1 *functional_form_LR = new TF1("functional_form_LR", "TMath::Sqrt(2*(1/([0]*x))+([1]/x)^2)", 0., 100.);
+
+    //peparing histogram for Npe e SigNoise distribution
+    CryNpe = tfdir_alr.make<TH1F>("Npe_distr", "Distribution of Npe of all crystalss", 120, 0., 60.);
+    CrySigNoise = tfdir_alr.make<TH1F>("SigNoise_distr", "Distribution of SigNoise of all crystals", 80, 0., 0.4);
+    CryNpeChi2= tfdir_alr.make<TH1F>("CryNpeChi2", "Distribution of #chi2/ndof of all crystals", 100, 0.,50);
+
+    // initializating string for npe-noise file dat
+    _outputfileAsym << "#Cry #Npe/MeV #Npe/MeVerr #Noise #Noiseerr \n\n";
+
+    //preparing variables to make tgraph for every crystal  
+    for(int icry=0; icry< nDisks*nCrystals; icry++){
+      for(int ibin=0;ibin< Ebin; ibin++){
+	CryALR[icry][ibin]->Fit("gaussianFit", "Q", "", -1.,1.);
+	CrySigmaALR[icry][ibin] = gaussianFit->GetParameter(2);
+	CrySigmaALRErr[icry][ibin] = gaussianFit->GetParError(2);
+
+	//avoid infinities due low statistics 
+	if(Cry_counter_energy_band[icry][ibin]==0){
+	  Cry_counter_energy_band[icry][ibin]=1;
+	}
+	Cryeband_mean[icry][ibin]=Cry_Energy_band[icry][ibin]/Cry_counter_energy_band[icry][ibin];
+	CryEneErr[icry][ibin]= 1/TMath::Sqrt(Cry_counter_energy_band[icry][ibin]); 
+	
+      }//end ibin for
+
+      //tgraph for single chanel
+      CryNpeALR[icry] = tfdir_cryalr.make<TGraphErrors>(Ebin, Cryeband_mean[icry], CrySigmaALR[icry], CryEneErr[icry], CrySigmaALRErr[icry]);
+      CryNpeALR[icry]->SetMarkerStyle(8);
+      CryNpeALR[icry]->SetMarkerSize(0.9);
+      functional_form_ALR->SetParameters(30., 0.2);
+
+      //fit single channel
+      CryNpeALR[icry]->Fit("functional_form_ALR","Q");
+
+      //fill npe-nois file dat with crsystal; npe/mev +/- err; noise +/- err
+      _outputfileAsym << Form("%i %.3f %.3f %.3f %.3f\n", icry, functional_form_ALR->GetParameter(0), functional_form_ALR->GetParError(0), functional_form_ALR->GetParameter(1), functional_form_ALR->GetParError(1)); 
+      //eventualy chi2/ndf cut 
+      if(functional_form_ALR->GetChisquare()/functional_form_ALR->GetNDF() > 0){
+      CryNpeALR[icry]->Write(Form("Cry_%i_NPe", icry));
+      CryNpe->Fill(functional_form_ALR->GetParameter(0));//fill Npe
+      CryNpe->Fit("gaus", "Q");
+      CrySigNoise->Fill(functional_form_ALR->GetParameter(1)); //fill SigNoise
+      CrySigNoise->Fit("gaus", "Q");
+      }
+      CryNpeChi2->Fill(functional_form_ALR->GetChisquare()/functional_form_ALR->GetNDF());
+    }//end icry for
+
+    _outputfileAsym.close();
+    for(int j=0; j<Ebin; j++){
+      eband_mean[j] = Energy_band[j]/counter_energy_band[j];
+      errEne[j]=1/TMath::Sqrt(counter_energy_band[j]);
+
+      LR[j]->Fit("gaussianFit", "Q", "", 0.5, 1.5);
+      sigmaLR[j]= gaussianFit->GetParameter(2);
+      sigmaLRerr[j]= gaussianFit->GetParError(2);
+      NpeLRvar[j] = 2/gaussianFit->GetParameter(2)/gaussianFit->GetParameter(2)/eband_mean[j];
+      errLR[j] = 4*gaussianFit->GetParError(2)/gaussianFit->GetParameter(2)/gaussianFit->GetParameter(2)/gaussianFit->GetParameter(2)/eband_mean[j];
+
+      ALR[j]->Fit("gaussianFit","Q", "", -1., 1.);
+      sigmaALR[j]= gaussianFit->GetParameter(2);
+      sigmaALRerr[j]= gaussianFit->GetParError(2);
+      NpeALRvar[j] = 1/gaussianFit->GetParameter(2)/gaussianFit->GetParameter(2)/eband_mean[j]/2;
+      errALR[j] = 0.5*gaussianFit->GetParError(2)/gaussianFit->GetParameter(2)/gaussianFit->GetParameter(2)/gaussianFit->GetParameter(2)/eband_mean[j];
+    }
+
+    //analsis for all cristals togheter
+    NpeLR = tfdir_alr.make<TGraphErrors>(Ebin, eband_mean, NpeLRvar, errEne, errLR);
+    NpeALR = tfdir_alr.make<TGraphErrors>(Ebin, eband_mean, NpeALRvar, errEne, errALR);
+    sigmaELR = tfdir_alr.make<TGraphErrors>(Ebin, eband_mean, sigmaLR, errEne, sigmaLRerr);
+    sigmaEALR = tfdir_alr.make<TGraphErrors>(Ebin, eband_mean, sigmaALR, errEne, sigmaALRerr);
+
+    NpeLR->SetMarkerSize(0.9);
+    NpeLR->SetMarkerStyle(8);
+    NpeALR->SetMarkerSize(0.9);
+    NpeALR->SetMarkerStyle(8);
+    sigmaELR->SetMarkerSize(0.9);
+    sigmaELR->SetMarkerStyle(8);
+    sigmaEALR->SetMarkerSize(0.9);
+    sigmaEALR->SetMarkerStyle(8);
+    NpeLR->Fit("pol0", "Q");
+    NpeALR->Fit("pol0", "Q");
+    functional_form_LR->SetParameters(30., 0.2);
+    functional_form_ALR->SetParameters(30., 0.2);
+    sigmaELR->Fit("functional_form_LR", "Q");
+    sigmaEALR->Fit("functional_form_ALR", "Q");
+    NpeLR->Write("NpeLR");
+    NpeALR->Write("NpeALR");
+    sigmaELR->Write("sigmaELR");
+    sigmaEALR->Write("sigmaEALR");
+
+    if(_diagLevel>0){
+      std::cout << "[0]: " << functional_form_ALR->GetParameter(0)  << "[1]: " << functional_form_ALR->GetParameter(1);
+      std::cout << "[0]: " << functional_form_LR->GetParameter(0)  << "[1]: " << functional_form_LR->GetParameter(1);
+    }
+
+    delete gaussianFit;
+    delete functional_form_ALR;
+    delete functional_form_LR;
+    for(int icry=0; icry< nDisks*nCrystals; icry++){
+      for(int ibin=0; ibin < Ebin; ibin++){
+	delete CryALR[icry][ibin];
+      }
+    }
 
   }//end endjob
  double CaloCosmicEnergy::langaufun(double *x, double *par) {
