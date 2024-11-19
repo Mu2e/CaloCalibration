47a08d79a46e276f814e30bd8f75817c3ed90d54
sophieMu2e <sophie@fnal.gov>
2024-11-19 09:50:31 -0600

    adc works

---
+++ SourceCalib/CaloEventDisplay_module.cc
@@ -0,0 +1,192 @@
+// C++ includes.
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <list>
+#include <deque>
+//Mu2e Geom:
+#include "CalorimeterGeom/inc/Calorimeter.hh"
+#include "CalorimeterGeom/inc/DiskCalorimeter.hh"
+#include "TrackerGeom/inc/Tracker.hh"
+
+#include "GeometryService/inc/GeomHandle.hh"
+#include "GeometryService/inc/GeometryService.hh"
+#include "GeometryService/inc/VirtualDetector.hh"
+#include "GeometryService/inc/DetectorSystem.hh"
+
+//Mu2e Data Prods:
+#include "RecoDataProducts/inc/CaloCrystalHitCollection.hh"
+#include "RecoDataProducts/inc/CaloHitCollection.hh"
+#include "RecoDataProducts/inc/CaloHit.hh"
+#include "RecoDataProducts/inc/CaloCluster.hh"
+#include "RecoDataProducts/inc/CaloClusterCollection.hh"
+
+// Mu2e Utilities
+#include "GeometryService/inc/GeomHandle.hh"
+
+// Framework includes.
+#include "art/Framework/Core/EDAnalyzer.h"
+#include "art/Framework/Principal/Event.h"
+#include "art/Framework/Principal/Handle.h"
+#include "art_root_io/TFileService.h"
+#include "art/Framework/Core/ModuleMacros.h"
+
+
+// ROOT incldues
+#include "TLegend.h"
+#include "TLatex.h"
+#include "TTree.h"
+#include "TH2D.h"
+#include "TF1.h"
+#include "TH3D.h"
+#include "Rtypes.h"
+#include "TApplication.h"
+#include "TArc.h"
+#include "TTUBE.h"
+#include "TBox.h"
+#include "TCanvas.h"
+#include "TDirectory.h"
+#include "TGraph.h"
+#include "TH1F.h"
+#include "TLine.h"
+#include "TNtuple.h"
+#include "TPolyMarker.h"
+#include "TPolyMarker3D.h"
+#include "TPolyLine3D.h"
+#include "TStyle.h"
+#include "TText.h"
+#include "TRotMatrix.h"
+
+
+using namespace std; 
+
+namespace mu2e 
+{
+  class CaloEventDisplays : public art::EDAnalyzer {
+    public:
+	    struct Config{
+        using Name=fhicl::Name;
+        using Comment=fhicl::Comment;
+        fhicl::Atom<art::InputTag> calocrysTag{Name("CaloCrystalHitCollection"),Comment("cal reco crystal hit info")};
+        fhicl::Atom<art::InputTag> caloclusterTag{Name("CaloClusterCollection"),Comment("cal reco cluster info")};
+        fhicl::Atom<bool> doDisplay{Name("doDisplay"),Comment("use display"), false};
+        fhicl::Atom<bool> clickToAdvance{Name("clickToAdvance"),Comment("next event"), false};
+	     };
+      typedef art::EDAnalyzer::Table<Config> Parameters;
+
+      explicit CaloEventDisplays(const Parameters& conf);
+
+      virtual ~CaloEventDisplays();
+      virtual void beginJob();
+      virtual void analyze(const art::Event& e) override;
+    private: 
+	    Config _conf;
+
+	    
+	    //For Event Displays:
+	    TApplication* application_;
+	    TDirectory*   directory_ = nullptr;
+	    TCanvas*      canvas_ = nullptr;
+	    TH2D* _display = nullptr;
+	    TNtuple* _ntTrack = nullptr;
+	    TNtuple* _ntHit = nullptr;
+
+	    
+      art::InputTag _calocrysTag;
+      art::InputTag _caloclusterTag;
+
+      const CaloCrystalHitCollection*  _calcryhitcol;
+      const CaloClusterCollection* _calclustercol;
+
+    };
+
+    CaloEventDisplays::CaloEventDisplays(const Parameters& conf) :
+    art::EDAnalyzer(conf),
+    _calocrysTag(conf().calocrysTag()),
+    _caloclusterTag(conf().caloclusterTag()),
+    doDisplay_ (conf().doDisplay()),
+    clickToAdvance_ (conf().clickToAdvance())
+    {}
+   
+    CaloEventDisplays::~CaloEventDisplays(){}
+
+    void CaloEventDisplays::beginJob() {
+      
+      art::ServiceHandle<art::TFileService> tfs;
+      directory_ = gDirectory;
+
+      TString name  = "test";
+      TString title = "test";
+      int window_size_x(1300);
+      int window_size_y(600);
+      canvas_ = tfs->make<TCanvas>(name,title,window_size_x,window_size_y);
+      canvas_->Divide(1,2);
+   
+  }
+
+  void CaloEventDisplays::analyze(const art::Event& event) {
+	 
+  art::ServiceHandle<GeometryService> geom;
+  if( ! geom->hasElement<Calorimeter>() ) return;
+  Calorimeter const & cal = *(GeomHandle<Calorimeter>());
+
+    TText  text;     
+    TArc arcOut, arcIn;
+    TBox box;
+    canvas_->SetTitle("foo title");
+    auto pad = canvas_->cd();
+    pad->Clear();
+    canvas_->SetTitle("bar title");
+
+    auto xyplot = pad->DrawFrame(-1000,-1000, 1000,1000);
+    xyplot->GetYaxis()->SetTitleOffset(1.25);
+    xyplot->SetTitle( "View of Calo Disk 1 in YZ Plane; Z(mm);Y(mm)");
+
+    art::ServiceHandle<mu2e::GeometryService>   geom;
+
+    mu2e::GeomHandle<mu2e::DetectorSystem>      ds;
+    mu2e::GeomHandle<mu2e::VirtualDetector>     vdet;
+
+    Hep3Vector vd_tt_back = ds->toDetector(vdet->getGlobal(mu2e::VirtualDetectorId::TT_Back));
+
+    Disk const & disk =  cal.disk(1);
+    double outerR = disk.outerRadius();
+    double innerR= disk.innerRadius();
+
+    arcOut.SetFillColor(kGray);
+    arcIn.SetFillColor(kWhite);
+    arcIn.SetLineColor(kGray+1);
+    arcOut.SetLineColor(kGray+1);
+    arcOut.DrawArc(0.,0., outerR);
+    arcIn.DrawArc(0.,0., innerR);
+
+    unsigned int i =0;
+    for(int i=0;i < 674;i++){
+        int crystalID(i);
+	      Crystal const &crystal = cal.crystal(i);
+        double crystalXLen = crystal.size().x();
+        double crystalYLen = crystal.size().y();
+        CLHEP::Hep3Vector crystalPos   = cal.geomUtil().mu2eToDiskFF(1,crystal.position());
+        box.SetLineColor(kGray+1);
+        box.DrawBox(crystalPos.x()-crystalXLen/2, crystalPos.y()-crystalYLen/2,crystalPos.x()+crystalXLen/2, crystalPos.y()+crystalYLen/2);
+  int diskId     = cal.crystal(hit->id()).diskId();
+
+        CLHEP::Hep3Vector crystalPos   = cal.geomUtil().mu2eToDiskFF(diskId,cal.crystal(hit->id()).position());
+
+        box.DrawBox(crystalPos.x()-crystalXLen/2, crystalPos.y()-crystalYLen/2,crystalPos.x()+crystalXLen/2, crystalPos.y()+crystalYLen/2);
+     
+        TLatex latex;
+        stringstream crys;
+        crys<<round(crystalID);
+        const char* str_crys = crys.str().c_str();
+        latex.SetTextSize(0.02);
+        latex.DrawLatex(crystalPos.x()-crystalXLen/2, crystalPos.y()-crystalYLen/2,str_crys);
+      }
+  }
+
+
+}  // end namespace mu2e 
+
+using mu2e::CaloEventDisplays;
+DEFINE_ART_MODULE(CaloEventDisplays);
+
--- SourceCalib/src/MakeAnalysisTree_main.cc
+++ SourceCalib/src/MakeAnalysisTree_main.cc
@@ -7,7 +7,6 @@ using namespace CaloSourceCalib;
 
 TString filepath = "/pnfs/mu2e/tape/usr-nts/nts/sophie/SourceCalibSimAna/ADCCuts/root/56/e6/nts.sophie.SourceCalibSimAna.ADCCuts.0.root";
 
-//TString filepath = "/exp/mu2e/app/users/hjafree/SourceFitDir/mu2e_caloSimu_crysEdep_770.root";
 /*function to extract the TTree from the SourceCalibAna output*/
 TH1F* get_data_histogram(int cryNum){
     TFile *f =  new TFile(filepath);
@@ -16,119 +15,17 @@ TH1F* get_data_histogram(int cryNum){
     return hist;
 }
 
-TString truthfilepath = "/pnfs/mu2e/tape/usr-nts/nts/hjafree/SourceCalibAna/MDC2020ae/root/0a/1f/nts.hjafree.SourceCalibAna.MDC2020ae.0.root"; 
-
-/*function to extract the TTree from the SourceCalibAna output*/
-TTree* get_data_tree(){
-    TFile *f =  new TFile(truthfilepath);
-    TTree *t = (TTree*)f->Get("CaloExample/Calo");
-    return t;
-}
-//new function void-- loop over ttree (use above func to get ttree) use t2hf func to get histogram-- bin width is 674 bins 
-/* bin for a given crystal */
-void AnalyzeTruthCrystal(int crystalNo){
-
-    TString cryNum;
-    cryNum = to_string(crystalNo);
-
-    // create the output file.
-    TString trueouptName = "mu2e_caloSimu_crysEdep_" + cryNum + ".root";
-    TFile *trueouptFile = new TFile(trueouptName, "RECREATE");
-    //create output tree
-    Float_t trueEdep;//crysEdep, ratio, ntrig, stim, time, tErg;
-    TTree *outTree = new TTree("trueEdep","trueEdep");
-    outTree->Branch("trueEdep", &trueEdep, "trueEdep/F"); //True Energy Deposited
-//ToDO change things to make sure correct branch names
-    // make histogram
-    TH1F *true_spec = new TH1F("simEdep", "simEdep", 300, 0.0, 7.5);
-    // input tree
-    TTree *inTree = get_data_tree();
-    // extract branches - these are arrays as there will be multiple crystal hits per event
-    int  nCry;//number of crystal hits in event
-    int cryId[10];
-    float cryTime[10];
-    float cryPosX[10], cryPosY[10], cryPosZ[10];
-    float cryEdep[10];
-    float simEdep[10];
-    inTree -> SetBranchAddress("nCry", &nCry);//ncalhitHit
-    inTree -> SetBranchAddress("cryId", &cryId);//same
-    inTree -> SetBranchAddress("simEdep", &simEdep);//same
-    inTree -> SetBranchAddress("cryEdep", &cryEdep);//calhitRecoEdep
-    inTree -> SetBranchAddress("cryTime", &cryTime);//calhitRecoTime
-    inTree -> SetBranchAddress("cryPosX", &cryPosX);//calhitRecoPosX
-    inTree -> SetBranchAddress("cryPosY", &cryPosY);//calhitRecoPosY
-    inTree -> SetBranchAddress("cryPosZ", &cryPosZ);//calhitRecoPosZ
-   
-    //unsigned int nEvt = (int)inTree -> GetEntries();
-    unsigned int nEvtCrys = 0; //events in this crystal
-    for(unsigned int iEvt=0; iEvt<1e7; iEvt++)//1e7
-    {
-      // extract single entry in the Tree
-      inTree -> GetEntry(iEvt);
-      
-      int idExist = 0;
-      std::vector<float> sameCryTime;
-      std::vector<float> sameCryEdep;
-      
-      for(int icry=0; icry<nCry; icry++)
-      {
-        
-        if(cryId[icry] == crystalNo)
-        {
-          idExist += 1;
-          sameCryTime.push_back(cryTime[icry]);
-          sameCryEdep.push_back(simEdep[icry]);
-        }
-      }
-
-      if(idExist == 0) continue;
-      sort(sameCryTime.begin(), sameCryTime.end());
-      float edepTarget = 0.0;
-      float edepOthers   = 0.0;
-
-      std::vector<float> deltaTime;
-
-      for(int icry=0; icry<nCry; icry++)
-      {
-        if(cryId[icry] == crystalNo)
-        {
-          edepTarget = simEdep[icry];
-          deltaTime.push_back(cryTime[icry]);
-
-        }
-        else
-        {
-          edepOthers += simEdep[icry];
-          deltaTime.push_back(cryTime[icry]);
-        }
-      }
-      sort(deltaTime.begin(), deltaTime.end());
-      float difTime = deltaTime.back() - deltaTime.front();
-      
-      if((edepTarget / (edepTarget + edepOthers)) >= 0.8 && difTime < 4)//0.8, 4
-      {
-        trueEdep =edepTarget;
-        true_spec->Fill(edepTarget);
-        nEvtCrys+=1;
-      }
-      outTree->Fill();
-      
-    }
-    std::cout<<" Events analyzed in this crystal : "<<nEvtCrys<<std::endl;
-  trueouptFile -> Write();
-  trueouptFile -> Close();
-
-}
-  /*function calls RooFit and fits a given crystal file*/
-void RunRooFit(int crystalNo) {
+/*function calls RooFit and fits a given crystal file*/
+/*void RunRooFit(int crystalNo) {
   SourceFitter *fit = new SourceFitter();
-  fit->MCFitCrystal(crystalNo,"chi2_MC");
-}
+  fit->FitCrystal(crystalNo,"chi2_MC");
+}*/
+
 /*main function allows a loop over all crystals or a choice of a single crystal*/
 int main(int argc, char* argv[]){
   std::cout<<"========== Welcome to the Mu2e Source Calibration Analysis =========="<<std::endl;
-  int anacrys_start = 770; //starting crystal//675
-  int anacrys_end = 771; //final crystal//680
+  int anacrys_start = 674; //starting crystal//675
+  int anacrys_end = 1348; //final crystal//680
   std::cout<<"choosing crystal "<<std::endl;
   if(strcmp( argv[1], "chooseCrystal") == 0 ){
     cout<<"crystal to be analyzed (int) : "<<endl;
@@ -136,9 +33,6 @@ int main(int argc, char* argv[]){
     anacrys_end = anacrys_start+1;
 
   }
-
-  int debug = 0;
-  if (debug == 0) {
  
 		TFile *ouptFile = new TFile("paraFile.root", "RECREATE");
 		Float_t fpeak, fsigma, chiSq, fstpeak, fstsigma, scdpeak,scdsigma,fcbalphaparam,fcbndegparam,Aparam,Bparam,Cparam,fullResparam,fstResparam,scdResparam,comCnstparam,
@@ -167,60 +61,22 @@ int main(int argc, char* argv[]){
 		covar->Branch("frBKG", &frBKGparam,"frBKGparam/F");
 		covar->Branch("crystalNo", &crystalNoparam,"crystalNoparam/F");
 		
-	 for(int cryNum=770; cryNum<771; cryNum++){//674 --> 1348 
-		TH1F* h = get_data_histogram(cryNum);
-		std::cout<<"start value"<<anacrys_start<<std::endl;
-		std::cout<<"end value"<<anacrys_end<<std::endl;
-		std::cout<<"Running crystal binning ....."<<std::endl;
-		//MakeCrystalListOutputs(anacrys_start,anacrys_end);
-		//MakeCrystalBinsOutputs(anacrys_start,anacrys_end);
-		SourceFitter *fit = new SourceFitter();
-		fit->FitCrystal(h,"chi2", cryNum, covar, fpeak,fsigma,chiSq, fstpeak, fstsigma, scdpeak,scdsigma,fcbalphaparam,fcbndegparam,Aparam,Bparam,Cparam,
-		fullResparam,fstResparam,scdResparam,comCnstparam,combetaparam,
-		frFullparam,frFrstparam,frScndparam,crystalNoparam,frBKGparam);//add fractions frBKGparam
-	 };
+	 for(int cryNum=anacrys_start; cryNum<anacrys_end; cryNum++){
+		  TH1F* h = get_data_histogram(cryNum);
+		  std::cout<<"Running fitter ....."<<std::endl;
+		  auto start_bin = high_resolution_clock::now();
+    
+		  SourceFitter *fit = new SourceFitter();
+		  fit->FitCrystal(h,"chi2", cryNum, covar, fpeak,fsigma,chiSq, fstpeak, fstsigma, scdpeak,scdsigma,fcbalphaparam,fcbndegparam,Aparam,Bparam,Cparam,
+		  fullResparam,fstResparam,scdResparam,comCnstparam,combetaparam,
+		  frFullparam,frFrstparam,frScndparam,crystalNoparam,frBKGparam);
+		  
+		  auto end_bin = high_resolution_clock::now();
+      std::cout<<" Time take to fit crystal "<<cryNum<<" "<<duration_cast<seconds>(end_bin - start_bin)<<" s "<<std::endl;
+	  };
 	 	ouptFile -> Write();
 		ouptFile -> Close();
 
 		std::cout<<"Finished processing ..."<<std::endl;
-		};
-		if (debug == 1){
-			for(int crystalNo = 770; crystalNo < 771; crystalNo++){
-    AnalyzeTruthCrystal(crystalNo);
-    RunRooFit(crystalNo);
-}
-			};
-/*	if (debug == 2){
-			
-		TFile *ouptFile = new TFile("paraFile.root", "RECREATE");
-		Float_t fpeak, fsigma, chiSq,fcbalphaparam,fcbndegparam,comCnstparam,
-		combetaparam,crystalNoparam;
-		TTree *covar = new TTree("covar","Covariance Plot");
-		covar->Branch("Peak", &fpeak,"fpeak/F");
-		covar->Branch("Width", &fsigma,"fsigma/F");
-		covar->Branch("ChiSq", &chiSq,"chiSq/F");
-		covar->Branch("Alpha", &fcbalphaparam,"fcbalphaparam/F");
-		covar->Branch("Ndeg", &fcbndegparam,"fcbndegparam/F");
-		covar->Branch("comCnst", &comCnstparam,"comCnstparam/F");
-		covar->Branch("combeta", &combetaparam,"combetaparam/F");
-		covar->Branch("crystalNo", &crystalNoparam,"crystalNoparam/F");
-		
-		for(int cryNum=770; cryNum<771; cryNum++){//674 --> 1348 
-		TH1F* h = get_data_histogram(cryNum);
-		std::cout<<"start value"<<anacrys_start<<std::endl;
-		std::cout<<"end value"<<anacrys_end<<std::endl;
-		std::cout<<"Running crystal binning ....."<<std::endl;
-		//MakeCrystalListOutputs(anacrys_start,anacrys_end);
-		//MakeCrystalBinsOutputs(anacrys_start,anacrys_end);
-		SourceFitter *fit = new SourceFitter();
-		fit->CBFitCrystal(h,"chi2", cryNum, covar, fpeak,fsigma,chiSq,fcbalphaparam,fcbndegparam,
-		comCnstparam,combetaparam,crystalNoparam);//add fractions
-	 };
-	 	ouptFile -> Write();
-		ouptFile -> Close();
-
-		std::cout<<"Finished processing ..."<<std::endl;
-		
-}*/
   return 0;
 }
