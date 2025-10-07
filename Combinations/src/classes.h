43a811c66df5e85b43f0f257484cbd6cf2cbdad0
sophieMu2e <sophie@fnal.gov>
2025-10-07 13:30:13 -0500

    added MC truth produ t

--- Combinations/src/classes.h
+++
@@ -1,4 +0,0 @@
-#include "CaloCalibration/SourceCalib/inc/MakeAnalysisTree.hh"
-#include "CaloCalibration/SourceCalib/inc/TableMaker.hh"
-#include "CaloCalibration/SourceCalib/inc/SourceFitter.hh"
-#include "CaloCalibration/SourceCalib/inc/SourcePlotter.hh"
